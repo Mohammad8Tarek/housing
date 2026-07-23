059e6cf01ff88649d852c4b36f9e570fdc2ce085
unknown <www.medo.mt90@gmail.com>
2026-07-23 20:06:46 +0300

    Fix TS2353: Add assignedRoomNumber to initial state

--- artifacts/housing/src/pages/hosting-requests/EditHostingRequest.tsx
+++ artifacts/housing/src/pages/hosting-requests/EditHostingRequest.tsx
@@ -39,6 +39,7 @@ export default function EditHostingRequest() {
     clockNumber: "",
     visitHotelId: "",
     numberOfRooms: "",
+    assignedRoomNumber: "",
     familyMembersCount: "",
     familyMembersIncluded: "",
     fromDate: "",
